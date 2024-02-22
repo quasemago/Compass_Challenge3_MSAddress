6ba57f7268becc1be8db24ecf8f8f677d50b5290
quasemago <brunoronningfn@gmail.com>
2024-02-21 21:20:17 -0400

    Projeto java Spring Boot inicial contendo todas as dependências para uso posterior criado.

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/MSAddressApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.msaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSAddressApplication.class, args);
+    }
+
+}
